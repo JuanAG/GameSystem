5e7254f952a646946a9824b3d58c380279d6fe90
JuanAG <jamespond402@gmail.com>
2017-04-26 18:35:40 +0200

    Updated index

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<?php echo file_get_contents("/PHP/Header.php") ?>
+<?php echo file_get_contents("\PHP\Header.php") ?>
 
         <div id="main" class="">
             <div id="slider" class="">
-
+                Espero que se vea algo
             </div>
             <div id="listProducts" class="">
 
             </div>
         </div>
 
-<?php echo file_get_contents("/PHP/Footer.php") ?>
+<?php echo file_get_contents("\PHP\Footer.php") ?>
