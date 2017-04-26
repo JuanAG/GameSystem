5a44d8667daca113d9b12b676410d699b57cd3b8
JuanAG <jamespond402@gmail.com>
2017-04-26 19:56:22 +0200

    Updated index

--- index.php
+++ index.php
@@ -132,7 +132,10 @@ switch ($language) {
 
             </div>
             <div id="finalIcons" class="">
-
+                <img id="nintendo3DS" class="" src="/Images/Icons/3DS.png" />
+                <img id="logo" class="" src="/Images/Icons/ps4.png" />
+                <img id="logo" class="" src="/Images/Icons/Xbox.png" />
+                <img id="logo" class="" src="/Images/Icons/paypal.png" />
             </div>
         </div>
     </body>
