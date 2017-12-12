53c1eb74b21def655c35ac50d2c3df50bb7ee6bc
JuanAG <jamespond402@gmail.com>
2017-12-12 01:42:21 +0100

    Done!

---
+++ cartAction.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+require_once 'PHP/DataBase.php';
+
+if(isset($_POST['name']) && $_POST['name'] != null) {
+
+    setUser($_POST['name'], $_POST['lastName'], $_POST['address'], $_POST['cp'], $_POST['dni'], $_POST['mail'], $_POST['login'], $_POST['pass']);
+
+    // Cookies Remenber me
+    if(isset($_POST['remenberMe'])){
+        setcookie("loginUser", $_POST['login'], time()+getCookieTime(), '/');
+    }
+}
