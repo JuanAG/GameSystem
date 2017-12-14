5ed2759d6df0b81477b9eb21d85950af2d8cf921
Juan Alcántara <jamespond402@gmail.com>
2017-12-14 10:16:12 +0100

    Update MagicConstants.php

--- PHP/MagicConstants.php
+++ PHP/MagicConstants.php
@@ -3,7 +3,7 @@
 function getSliderNumberOfGames(){return 4;}
 function getNumberOfTopNewGames(){return 12;}
 function getNumberOfTopFutureGames(){return 4;}
-function getQuickSearchMaxResults(){return 3;}
+function getQuickSearchMaxResults(){return 4;}
 function getGamesPhotos(){return 5;}
 
 function getMaxQueryResults(){return 24;}
